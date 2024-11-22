374382d575ed6ea65909a5d5e8a49308ca7de03c
Daniel Fimaini <daniel.fimiani@mercadolibre.com>
2024-11-22 13:10:16 -0300

    fix:restar input

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -95,5 +95,6 @@ public class GameManager : MonoBehaviour {
     public void RestartGame() {
         Time.timeScale = 1;
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        playerInput.ActivateInput();
     }
 }
